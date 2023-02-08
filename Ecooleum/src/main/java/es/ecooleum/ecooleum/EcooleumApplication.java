f497c262c175deeec85f3fac1144a80294f7b0d7
Raúl Heredia Horcajo <raulherediahorcajo@gmail.com>
2023-02-08 12:44:36 +0100

    chore: Add empty proyect

---
+++ Ecooleum/src/main/java/es/ecooleum/ecooleum/EcooleumApplication.java
@@ -0,0 +1,13 @@
+package es.ecooleum.ecooleum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcooleumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcooleumApplication.class, args);
+    }
+
+}
---
+++ Ecooleum/src/test/java/es/ecooleum/ecooleum/EcooleumApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ecooleum.ecooleum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcooleumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
